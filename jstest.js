2eccee7af541b25d18a6cbfab9c02759dec9266e
vmutest <117527832+vmutest@users.noreply.github.com>
2022-11-21 11:57:11 +0700

    Update jstest.js

--- jstest.js
+++ jstest.js
@@ -1,6 +1,6 @@
 window.addEventListener('load',function(){
   var q=$('.question'),
-  w=["A+ - Xuất sắc","B+ - Khá ","C+ - Trung Bình "," Chuẩn bị tiền đóng học lại thôi !!!"]
+  w=["A+","A","B+","B","C+","C","D+","D","F"]
   $(q).each(function(n){
     var ul=$(q[n]).find('ul'),mes=$(q[n]).parent().find('.message')
     console.log(mes)
@@ -16,10 +16,15 @@ window.addEventListener('load',function(){
           v='<span class="vmu">'+corr+'</span><span class="total">'+ul.length+'</span>',
           $(v).insertAfter($(mes))
           w.forEach(function(){
-            if(s==100)$(mes).html(w[0])
-            else if(s>=80)$(mes).html(w[1])
-            else if(s>=50)$(mes).html(w[2])
-            else $(mes).html(w[3])
+            if(s>=90)$(mes).html(w[0])
+            else if(s>=85)$(mes).html(w[1])
+            else if(s>=80)$(mes).html(w[2])
+            else if(s>=70)$(mes).html(w[3])
+            else if(s>=65)$(mes).html(w[4])
+            else if(s>=55)$(mes).html(w[5])
+            else if(s>=50)$(mes).html(w[6])
+            else if(s>=40)$(mes).html(w[7])
+            else $(mes).html(w[8])
           })
         }
       })
